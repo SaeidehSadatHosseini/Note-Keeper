70a6ec1294e8fc394e0308306481f5feb780af75
Saeideh Sadat Hosseini <saeideh.sadat.hosseini89@gmail.com>
2025-08-05 17:23:00 +0200

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [isExpand , setExpand]= useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function checkExpand(){
+   if (isExpand == false){
+    setExpand(true);
+
+   }
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+          style={ {display : isExpand ? "inline-block" : "none"}}
+        />
+        <textarea
+          onClick = {checkExpand}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpand ? 3 : 1}
+        />
+        <Zoom in={isExpand ? true : false}>
+          <Fab onClick= {submitNote}>
+
+          <AddIcon />
+        </Fab>
+        </Zoom>
+        
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
+
+function Header() {
+  return (
+    <header>
+      <h1><PlaylistAddCheckIcon/> Note Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
